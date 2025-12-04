369689e14098de72e0ad72ff78996027a565ed0a
Wojtmic <mail@wojtmic.dev>
2025-12-04 19:08:42 +0100

    Keep track of ran tweaks

--- prefixer/core/helpers.py
+++ prefixer/core/helpers.py
@@ -11,6 +11,10 @@ def setup_env(ctx: RuntimeContext):
     env['STEAM_COMPAT_CLIENT_INSTALL_PATH'] = os.path.expanduser('~/.steam/steam')
     return env
 
+def add_tweak_to_file(file: str, tweak: str):
+    with open(file, 'a') as f:
+        f.write(tweak)
+
 def run_tweak(runtime: RuntimeContext, target_tweak: Tweak):
     tasks = target_tweak.tasks
 
@@ -18,3 +22,15 @@ def run_tweak(runtime: RuntimeContext, target_tweak: Tweak):
         click.echo(f'{click.style('==>', bold=True)} {task.description} {click.style(task.type, fg='bright_black')}')
         task.resolve_paths(runtime)
         task_registry[task.type](ctx=task, runtime=runtime)
+
+    ran_tweak_file = os.path.join(runtime.pfx_path, 'tweaks.prefixer.txt')
+
+    if not os.path.exists(ran_tweak_file):
+        add_tweak_to_file(ran_tweak_file, target_tweak.name)
+        return
+
+    with open(ran_tweak_file, 'r') as f:
+        content = f.read()
+        if target_tweak.name in content: return
+
+    add_tweak_to_file(ran_tweak_file, target_tweak.name)
