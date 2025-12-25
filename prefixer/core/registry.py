e9704cdc459fab2fc9823d7d09847d3e54762cd4
Wojtmic <mail@wojtmic.dev>
2025-12-25 13:38:21 +0100

    update registry for conditions

--- prefixer/core/registry.py
+++ prefixer/core/registry.py
@@ -1,8 +1,14 @@
 from typing import Callable
 
 task_registry = {}
+condition_registry = {}
 
 def task(func: Callable):
     """Registers a function as a task"""
     task_registry[func.__name__] = func
+    return func
+
+def condition(func: Callable):
+    """Registers a function as a condition"""
+    condition_registry[func.__name__] = func
     return func
